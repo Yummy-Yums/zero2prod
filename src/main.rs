f897936ce2cffe5a6bfb0376be41e971709153b2
Yummy-Yums <eliasderby90@gmail.com>
2025-06-17 17:03:35 +0000

    dockerize chp

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::fmt::format;
+use actix_web::{HttpRequest, Responder};
+use sqlx::{Connection, PgPool};
 use std::net::TcpListener;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use secrecy::ExposeSecret;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use sqlx::{Connection, PgConnection, PgPool};
+use tracing_log::LogTracer;
+use zero2prod::telemetry::get_subscriber;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     println!("{:?}", req);
@@ -15,19 +17,18 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
-// #[tokio::main]
-// async fn main() -> Result<(), std::io::Error> {
-//     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind");
-//     println!("Listening on port {:?}", listener.local_addr().unwrap().port());
-// 
-//     run(listener)?.await
-// }
-
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber(
+        "zero2prod".into(),
+        "info".into(),
+        std::io::stdout,
+    );
+    LogTracer::init().expect("Failed to set logger");
+    
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(
-            &configuration.database.connection_string()
+            &configuration.database.connection_string().expose_secret()
         )   
         .await
         .expect("Failed to connect to Postgres.");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,8 @@ use sqlx::types::chrono::Utc;
 use uuid::Uuid;
 use actix_web::{web, HttpResponse};
 use sqlx::{PgConnection, PgPool};
-
+use tracing::Instrument;
+use crate::telemetry::init_subscriber;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,27 +11,70 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    match sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now(),
-    )
-    .execute(pool.get_ref())
-    .await {
-        Ok(_) => HttpResponse::Ok().finish(),
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );
+    match insert_subscriber(&pool, &form).await 
+    {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!(
+                "Failed to execute query : {:?}",
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
     
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData
+) -> Result<(), sqlx::Error> {
+
+        sqlx::query!(
+            r#"
+                INSERT INTO subscriptions (id, email, name, subscribed_at)
+                VALUES ($1, $2, $3, $4)
+            "#,
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            Utc::now(),
+        )
+        .execute(pool)
+            .await
+            .map_err(|e| {
+                tracing::error!("Failed to execute query: {:?}", e);
+                e
+            })?;
+        Ok(())
 }
