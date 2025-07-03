b8d0adf55e94aaa2b4d147053f13e3e248fedb8f
Yummy-Yums <eliasderby90@gmail.com>
2025-07-03 17:59:18 +0000

    Dev (#7)
    * Empty commit message
    
    * Empty commit message
    
    * dockerize chp
    
    * reached subscripts_confirm
    
    * added auth routes
    
    * added password authorization
    
    * added login forms

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,8 +2,10 @@ mod health_check;
 mod subscriptions;
 mod subscriptions_confirm;
 mod newsletter;
+mod home;
 
 pub use health_check::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
 pub use newsletter::*;
+pub use home::*;
